45c78919111a169ebf65d8d08befb8930f5574e8
Srivatsav Raghu <41820878+vat0599@users.noreply.github.com>
2022-11-28 00:10:10 +0530

    Update Test_Anomaly_Detector_public.py

--- Anomaly Detection/Test_Anomaly_Detector_public.py
+++ Anomaly Detection/Test_Anomaly_Detector_public.py
@@ -123,33 +123,3 @@ for iv in range(nVideos):
     aa=aa[0:-4]
     A_predictions_path = Results_Path + aa + '.mat'  # Save array of 1*32, containing anomaly score for each segment. Please see Evaluate Anomaly Detector to compute  ROC.
     print "Total Time took: " + str(datetime.now() - time_before)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
